09c824d959186884a8bb863c471d041795738968
ChrisLTD <ChrisLTD@gmail.com>
2009-11-25 17:46:40 -0500

    display card working, dev server properly serving static files

--- generator/views.py
+++ generator/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, get_object_or_404
 from generator.models import Card, Picture, Bow, Typeface
 
 # Create your views here.
 
 def displaycard(request, cardid):
-	card = Card.objects.get(id=cardid)
+	card = get_object_or_404(Card, id=cardid)
 	return render_to_response('displaycard.html', locals())
 	
--- urls.py
+++ urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls.defaults import *
+from django.conf import settings
+from settings import *
 from generator.views import displaycard
+
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -14,6 +17,12 @@ urlpatterns = patterns('',
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    (r'^card/', displaycard),
+    (r'^card/(?P<cardid>\d+)/', displaycard),
     (r'^admin/', include(admin.site.urls)),
+    
 )
+
+if settings.DEBUG:
+	urlpatterns += patterns('',
+		(r'^images/(?P<path>.*)$', 'django.views.static.serve', {'document_root': os.path.join(SITE_ROOT, 'images')}),
+	)
