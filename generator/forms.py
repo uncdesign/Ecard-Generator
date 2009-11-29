7545d2f5d3a1d9b574d36c629f0800198da134ca
ChrisLTD <ChrisLTD@gmail.com>
2009-11-29 18:59:27 -0500

    Form options now display as radio buttons rather than drop downs

--- generator/forms.py
+++ generator/forms.py
@@ -1,7 +1,14 @@
-from django.forms import ModelForm
-from generator.models import Card
+from django.db import models
+from django.forms import ModelForm, ModelChoiceField
+from django.forms.widgets import RadioSelect
+from generator.models import Card, Picture, Bow, Typeface
 
 class CreateCardForm(ModelForm):
+	picture = ModelChoiceField(widget=RadioSelect, queryset=Picture.objects.all())
+	typeface = ModelChoiceField(widget=RadioSelect, queryset=Typeface.objects.all())
+	bow = ModelChoiceField(widget=RadioSelect, queryset=Bow.objects.all())
+	
+	
 	class Meta:
 		model = Card
 		fields = ['toemail', 'fromemail', 'message', 'typeface', 'picture', 'bow']
