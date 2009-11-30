a48a4bd711c03bf0e94449070ba597853c5bf825
ChrisLTD <ChrisLTD@gmail.com>
2009-11-29 20:14:30 -0500

    Radio select cleaned up

--- generator/forms.py
+++ generator/forms.py
@@ -2,11 +2,12 @@ from django.db import models
 from django.forms import ModelForm, ModelChoiceField
 from django.forms.widgets import RadioSelect
 from generator.models import Card, Picture, Bow, Typeface
+#from generator.widgets import *
 
 class CreateCardForm(ModelForm):
-	picture = ModelChoiceField(widget=RadioSelect, queryset=Picture.objects.all())
-	typeface = ModelChoiceField(widget=RadioSelect, queryset=Typeface.objects.all())
-	bow = ModelChoiceField(widget=RadioSelect, queryset=Bow.objects.all())
+	picture = ModelChoiceField(widget=RadioSelect, queryset=Picture.objects.all(), required=True, empty_label=None)
+	typeface = ModelChoiceField(widget=RadioSelect, queryset=Typeface.objects.all(), required=True, empty_label=None)
+	bow = ModelChoiceField(widget=RadioSelect, queryset=Bow.objects.all(), required=True, empty_label=None)
 	
 	
 	class Meta:
